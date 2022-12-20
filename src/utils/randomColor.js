5498942c9ed59090cfe632fb00cf0089af0ca791
Viktor Biletskyi <106250265+f0remangod@users.noreply.github.com>
2022-12-20 09:14:59 +0200

    randomHEX

--- src/utils/randomColor.js
+++ src/utils/randomColor.js
@@ -1,4 +1,5 @@
 export function randomColor() {
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  return '#' + randomColor;
+  // const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  // return '#' + randomColor;
+  return '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0');
 }
